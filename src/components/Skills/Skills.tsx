5a06316292029b8a158fe25b589f7a29d4f80b7e
Alexis MIALON <alexis.mialon@epita.fr>
2026-08-22 16:43:51 +0200

    style: change skills display

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -33,6 +33,23 @@ import { VscVscode } from "react-icons/vsc";
 import { motion } from "framer-motion";
 import "./Skills.css";
 
+type SkillIconProps = {
+    name: string;
+    children: React.ReactNode;
+};
+
+function SkillIcon({ name, children }: SkillIconProps) {
+    return (
+        <div
+            className="skill-icon"
+            data-tooltip={name}
+        >
+            {children}
+        </div>
+    );
+}
+
+
 function Skills() {
     return (
         <motion.section
@@ -56,11 +73,24 @@ function Skills() {
                 >
                     <h3>Operating Systems</h3>
 
+                    <br />
+
                     <div className="skill-icons">
-                        <SiLinux title="Linux" />
-                        <SiNixos title="NixOS" />
-                        <SiUbuntu title="Ubuntu" />
-                        <FaWindows title="Windows" />
+                        <SkillIcon name="Linux">
+                            <SiLinux />
+                        </SkillIcon>
+
+                        <SkillIcon name="NixOS">
+                            <SiNixos />
+                        </SkillIcon>
+
+                        <SkillIcon name="Ubuntu">
+                            <SiUbuntu />
+                        </SkillIcon>
+
+                        <SkillIcon name="Windows">
+                            <FaWindows />
+                        </SkillIcon>
                     </div>
                 </motion.div>
 
@@ -73,18 +103,52 @@ function Skills() {
                 >
                     <h3>Programming Languages</h3>
 
+                    <br />
+
                     <div className="skill-icons">
-                        <SiPython title="Python" />
-                        <SiC title="C" />
-                        <SiCplusplus title="C++" />
-                        <TbBrandCSharp title="C#" />
-                        <SiRust title="Rust" />
-                        <FaJava title="Java" />
-                        <SiJavascript title="JavaScript" />
-                        <SiTypescript title="TypeScript" />
-                        <FaHtml5 title="HTML" />
-                        <SiOcaml title="Ocaml" />
-                        <SiGnubash title="Bash" />
+                        <SkillIcon name="Python">
+                            <SiPython />
+                        </SkillIcon>
+
+                        <SkillIcon name="C">
+                            <SiC />
+                        </SkillIcon>
+
+                        <SkillIcon name="C++">
+                            <SiCplusplus />
+                        </SkillIcon>
+
+                        <SkillIcon name="C#">
+                            <TbBrandCSharp />
+                        </SkillIcon>
+
+                        <SkillIcon name="Rust">
+                            <SiRust />
+                        </SkillIcon>
+
+                        <SkillIcon name="Java">
+                            <FaJava />
+                        </SkillIcon>
+
+                        <SkillIcon name="JavaScript">
+                            <SiJavascript />
+                        </SkillIcon>
+
+                        <SkillIcon name="TypeScript">
+                            <SiTypescript />
+                        </SkillIcon>
+
+                        <SkillIcon name="HTML">
+                            <FaHtml5 />
+                        </SkillIcon>
+
+                        <SkillIcon name="Ocaml">
+                            <SiOcaml />
+                        </SkillIcon>
+
+                        <SkillIcon name="Bash">
+                            <SiGnubash />
+                        </SkillIcon>
                     </div>
                 </motion.div>
 
@@ -97,22 +161,68 @@ function Skills() {
                 >
                     <h3>Tools & Technologies</h3>
 
+                    <br />
+
                     <div className="skill-icons">
-                        <SiGit title="Git" />
-                        <SiGithub title="GitHub" />
-                        <SiGitlab title="Gitlab" />
-                        <SiDocker title="Docker" />
-                        <SiMysql title="MySQL" />
-                        <SiPostgresql title="PostgreSQL" />
-                        <SiLatex title="LaTeX" />
-                        <SiNumpy title="Numpy" />
-                        <SiPandas title="Pandas" />
-                        <SiReact title="React" />
-                        <VscVscode title="VScode" />
-                        <SiRider title="Rider" />
-                        <SiPycharm title="Pycharm" />
-                        <SiIntellijidea title="IntellijIdea" />
-                        <SiUnity title="Unity" />
+                        <SkillIcon name="Git">
+                            <SiGit />
+                        </SkillIcon>
+
+                        <SkillIcon name="GitHub">
+                            <SiGithub />
+                        </SkillIcon>
+
+                        <SkillIcon name="Gitlab">
+                            <SiGitlab />
+                        </SkillIcon>
+
+                        <SkillIcon name="Docker">
+                            <SiDocker />
+                        </SkillIcon>
+
+                        <SkillIcon name="MySQL">
+                            <SiMysql />
+                        </SkillIcon>
+
+                        <SkillIcon name="PostgreSQL">
+                            <SiPostgresql />
+                        </SkillIcon>
+
+                        <SkillIcon name="LaTeX">
+                            <SiLatex />
+                        </SkillIcon>
+
+                        <SkillIcon name="Numpy">
+                            <SiNumpy />
+                        </SkillIcon>
+
+                        <SkillIcon name="Pandas">
+                            <SiPandas />
+                        </SkillIcon>
+
+                        <SkillIcon name="React">
+                            <SiReact />
+                        </SkillIcon>
+
+                        <SkillIcon name="VScode">
+                            <VscVscode />
+                        </SkillIcon>
+
+                        <SkillIcon name="Rider">
+                            <SiRider />
+                        </SkillIcon>
+
+                        <SkillIcon name="Pycharm">
+                            <SiPycharm />
+                        </SkillIcon>
+
+                        <SkillIcon name="IntellijIdea">
+                            <SiIntellijidea />
+                        </SkillIcon>
+
+                        <SkillIcon name="Unity">
+                            <SiUnity />
+                        </SkillIcon>
                     </div>
                 </motion.div>
 
@@ -124,6 +234,8 @@ function Skills() {
                     transition={{ duration: 0.5, delay: 0.4 }}
                 >
                     <h3>Languages</h3>
+
+                    <br />
 
                     <div className="skill-icons">
                         <div className="language-item">
